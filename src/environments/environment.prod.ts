b1d17aa8eb5aac1594c461f4e62da4098c8d8e35
Java Monster <163273820+RenukaHiwale9284@users.noreply.github.com>
2024-06-21 15:45:04 +0530

    no chnage'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -33,7 +33,7 @@ export const environment = {
 
 
 
-
+  
   url1:'#{BACKEND_URL}#/api',
   url:'#{BACKEND_URL}#/api',
 
@@ -45,7 +45,7 @@ export const environment = {
   redirect_url: '#{IDAM_REDIRECT_URL}#',
   scope: '#{IDAM_SCOP}#',
   grant_type: '#{IDAM_GRANT_TYPE}#',
-  decret: '#{IDAM_SECRET}#',
+  secret: '#{IDAM_SECRET}#',
   access_token: '#{IDAM_ACCESS_TOKEN}#',
   user_info: '#{IDAM_USER_INFO}#',
 
@@ -54,7 +54,7 @@ export const environment = {
   reportId : '#{PB_REPORT_ID}#',
   tenant_id : '#{PB_TENANT_ID}#',
   clientId : '#{PB_CLIENT_ID}#',
-  clientdecret : '#{PB_CLIENT_SECRET}#',
+  clientSecret : '#{PB_CLIENT_SECRET}#',
   powerbiScope: '#{PB_SCOP}#',
   username: '',
   resource: '#{PB_RESOURCE}#',
